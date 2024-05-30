b9614ef6c7f5bc0fd6b83f4d54b2c45cb040afdb
ThaptaweeS <Thaptawee.somngam@gmail.com>
2024-05-30 19:28:27 +0700

    first commit

--- lib/page/tank/tank10-data/tank10.dart
+++ lib/page/tank/tank10-data/tank10.dart
@@ -37,196 +37,185 @@ class Tank10 extends StatelessWidget {
 }
 
 class Tank10Body extends StatefulWidget {
-  const Tank10Body({Key? key}) : super(key: key);
+  const Tank10Body({super.key});
 
   @override
-  _Tank10BodyState createState() => _Tank10BodyState();
+  State<Tank10Body> createState() => _P1DASHBOARDMAINState2();
 }
 
-class _Tank10BodyState extends State<Tank10Body> {
+class _P1DASHBOARDMAINState2 extends State<Tank10Body> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: SingleChildScrollView(
+        primary: false,
         padding: EdgeInsets.all(defaultPadding),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            DashboardHeader(),
+            InkWell(
+              onTap: () {
+                _showTextPopup(context);
+              },
+              child: ListTile(
+                leading: Icon(
+                  Icons.storage,
+                  size: 24.0,
+                  color: const Color.fromARGB(255, 255, 255, 255),
+                ),
+                title: Text(
+                  'Zinc Phosphate(PB-181X(6700L)) : Dashboard',
+                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
             SizedBox(height: defaultPadding),
-            ChartSection1(),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  flex: 3,
+                  child: Column(
+                    children: [
+                      Chart133(),
+                    ],
+                  ),
+                ),
+                SizedBox(width: defaultPadding),
+                Expanded(
+                  flex: 3,
+                  child: Chart13(),
+                ),
+              ],
+            ),
             SizedBox(height: defaultPadding),
-            ChartSection2(),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  flex: 3,
+                  child: Column(
+                    children: [
+                      Chart134(),
+                    ],
+                  ),
+                ),
+                SizedBox(width: defaultPadding),
+                Expanded(
+                  flex: 3,
+                  child: Chart135(),
+                ),
+              ],
+            ),
             SizedBox(height: defaultPadding),
-            ChartSection3(),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  flex: 3,
+                  child: Column(
+                    children: [
+                      Chart136(),
+                    ],
+                  ),
+                ),
+                SizedBox(width: defaultPadding),
+                Expanded(
+                  flex: 3,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        height: 140,
+                      ),
+                      ElevatedButton.icon(
+                        onPressed: () {
+                          CuPage = Page02Autobody();
+                          MainBodyContext.read<ChangePage_Bloc>()
+                              .add(ChangePage_nodrower());
+                        },
+                        icon: Icon(Icons.add_to_photos_outlined),
+                        label: Text('Data Input'),
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.white,
+                          backgroundColor: Color.fromARGB(255, 175, 184, 38),
+                          minimumSize:
+                              Size(120, 60), // Set the size of the button
+                        ),
+                      ),
+                      SizedBox(
+                        height: 25,
+                      ),
+                      ElevatedButton.icon(
+                        onPressed: () {
+                          CuPage = Tank10BodyPage();
+                          MainBodyContext.read<ChangePage_Bloc>()
+                              .add(ChangePage_nodrower());
+                        },
+                        icon: Icon(Icons.fact_check_outlined),
+                        label: Text('Data History'),
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.white,
+                          backgroundColor: Color.fromARGB(255, 15, 161, 130),
+                          minimumSize:
+                              Size(120, 60), // Set the size of the button
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
             SizedBox(height: defaultPadding),
-            ChartSection4(),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class DashboardHeader extends StatelessWidget {
-  const DashboardHeader({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () => _showTextPopup(context),
-      child: ListTile(
-        leading: Icon(
-          Icons.storage,
-          size: 24.0,
-          color: Colors.white,
-        ),
-        title: Text(
-          'Zinc Phosphate(PB-181X(6700L)) : Dashboard',
-          style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-        ),
-      ),
-    );
-  }
-
-  void _showTextPopup(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Detail'),
-          content: Text(
-            '',
-            style: TextStyle(fontSize: 13.0),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(),
-              child: Text('OK'),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Column(
+                    children: [
+                      Chart21(),
+                    ],
+                  ),
+                ),
+                SizedBox(width: defaultPadding),
+                Expanded(
+                  flex: 2,
+                  child: Column(
+                    children: [
+                      Chart25(),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ],
-        );
-      },
-    );
-  }
-}
-
-class ChartSection1 extends StatelessWidget {
-  const ChartSection1({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Expanded(flex: 3, child: Chart133()),
-        SizedBox(width: defaultPadding),
-        Expanded(flex: 3, child: Chart13()),
-      ],
-    );
-  }
-}
-
-class ChartSection2 extends StatelessWidget {
-  const ChartSection2({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Expanded(flex: 3, child: Chart134()),
-        SizedBox(width: defaultPadding),
-        Expanded(flex: 3, child: Chart135()),
-      ],
-    );
-  }
-}
-
-class ChartSection3 extends StatelessWidget {
-  const ChartSection3({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Expanded(flex: 3, child: Chart136()),
-        SizedBox(width: defaultPadding),
-        Expanded(
-          flex: 3,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(height: 140),
-              CustomButton(
-                onPressed: () {
-                  CuPage = Page02Autobody();
-                  MainBodyContext.read<ChangePage_Bloc>().add(ChangePage_nodrower());
-                },
-                icon: Icons.add_to_photos_outlined,
-                label: 'Data Input',
-                backgroundColor: Color.fromARGB(255, 175, 184, 38),
-              ),
-              SizedBox(height: 25),
-              CustomButton(
-                onPressed: () {
-                  CuPage = Tank10BodyPage();
-                  MainBodyContext.read<ChangePage_Bloc>().add(ChangePage_nodrower());
-                },
-                icon: Icons.fact_check_outlined,
-                label: 'Data History',
-                backgroundColor: Color.fromARGB(255, 15, 161, 130),
-              ),
-            ],
-          ),
         ),
-      ],
-    );
-  }
-}
-
-class ChartSection4 extends StatelessWidget {
-  const ChartSection4({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Expanded(flex: 2, child: Chart21()),
-        SizedBox(width: defaultPadding),
-        Expanded(flex: 2, child: Chart25()),
-      ],
-    );
-  }
-}
-
-class CustomButton extends StatelessWidget {
-  final VoidCallback onPressed;
-  final IconData icon;
-  final String label;
-  final Color backgroundColor;
-
-  const CustomButton({
-    Key? key,
-    required this.onPressed,
-    required this.icon,
-    required this.label,
-    required this.backgroundColor,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton.icon(
-      onPressed: onPressed,
-      icon: Icon(icon),
-      label: Text(label),
-      style: ElevatedButton.styleFrom(
-        foregroundColor: Colors.white,
-        backgroundColor: backgroundColor,
-        minimumSize: Size(120, 60),
       ),
     );
   }
+}
+
+void _showTextPopup(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('Detail'),
+        content: Text(
+            // 'Process : Fine Cleaner \nTank Capacity : 6000 Litters \nChemicals : FC-4360 \n:: Replenishing :: \nFC-4360= 6.6 kgs./1 pt\n FAl increase (1.1 g/l)\nFrequency of Checking : 4 Times/day ',
+            '',
+            style: TextStyle(fontSize: 13.0)),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('OK'),
+          ),
+        ],
+      );
+    },
+  );
 }
